f3382b9c164b365c134e279ed2bc26b69969a7e2
Bailey <bailmcca601@ed.amdsb.ca>
2016-11-22 13:36:16 -0500

    initial commit

---
+++ StarWarsTrench/StarWarsTrench/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace StarWarsTrench
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.initiationLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(65, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // initiationLabel
+            // 
+            this.initiationLabel.AutoSize = true;
+            this.initiationLabel.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.initiationLabel.ForeColor = System.Drawing.Color.White;
+            this.initiationLabel.Location = new System.Drawing.Point(65, 278);
+            this.initiationLabel.Name = "initiationLabel";
+            this.initiationLabel.Size = new System.Drawing.Size(0, 16);
+            this.initiationLabel.TabIndex = 1;
+            this.initiationLabel.Click += new System.EventHandler(this.initiationLabel_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 16;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.initiationLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Star Wars";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label initiationLabel;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ StarWarsTrench/StarWarsTrench/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StarWarsTrench
+{
+    public partial class Form1 : Form
+    {
+        Pen solidWhite, solidRed;
+        Brush whiteBrush;
+        bool initiate, xWing = true;
+        int cx, cy;
+        public Form1()
+        {
+            InitializeComponent();
+            solidWhite = new Pen(Color.White); solidRed = new Pen(Color.Red);
+            whiteBrush = new SolidBrush(Color.White);
+            initiationLabel.Text = "Confidential mission document, click\nhere to begin";
+            cx = this.Width / 2; cy = this.Height / 2;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {            
+            this.Refresh();
+        }
+
+        private void initiationLabel_Click(object sender, EventArgs e)
+        {
+            initiationLabel.Text = "";
+            pictureBox1.Dispose();
+            initiate = true;
+            this.Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Black);
+            if (initiate)
+            {
+                #region Draw Death Star
+                e.Graphics.DrawEllipse(solidWhite, cx - 100, cy - 100, 200, 200);
+                e.Graphics.DrawEllipse(solidWhite, cx - 70, cy - 70, 60, 60);
+                e.Graphics.DrawLine(solidWhite, 100, 215, 300, 215);
+                e.Graphics.DrawLine(solidRed, cx, cy - 80, 200, 215);
+                e.Graphics.DrawRectangle(solidWhite, cx - 10, cy - 100, 20, 20);
+                e.Graphics.DrawEllipse(solidRed, cx - 10, cy + 5, 20, 20);
+
+                e.Graphics.FillRectangle(whiteBrush, 5, 50, 10, 10);
+                #endregion
+
+                while (xWing)
+                {
+
+                }
+            }
+        }
+    }
+}
